a1d1632d248aad5da0b40d8eb9a704feb2c2ff6c
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-08-20 14:33:28 -0500

    Creamos una funcion para  determinar el ganador si es 'X', 'O' o es null. con una arreglo de 9 cuadrados y sus posibles formas de ganar.

--- src/index.js
+++ src/index.js
@@ -78,3 +78,23 @@ class Juego extends React.Component {
 // ========================================
 
 ReactDOM.render(<Juego />, document.getElementById("root"));
+
+function calcularGanador(cuadrados){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const[a,b,c] = lines[i];
+    if (cuadrados[a] && cuadrados[a] === cuadrados[b] && cuadrados[a] == cuadrados[c]) {
+      return cuadrados[a];
+    }
+  }
+  return null;
+}
